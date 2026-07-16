7cbb8962047b88ba8482b4da8645e24d83495d6e
Karim <karimdavoodi@gmail.com>
2026-07-16 17:07:29 -0400

    remove dead codes

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,39 +1,3 @@
-export interface Info {
-  masjidName: string;
-  masjidSlogan: string;
-  googleMapsUrl: string;
-  prayerTimeUrl: string;
-  pinMessage: string;
-  contact: {
-    phone: string[];
-    address: string;
-    email: string;
-    socialMedia: {
-      facebook: string;
-      instagram: string;
-      x: string;
-      youtube: string;
-      tiktok: string;
-    };
-  };
-  usefulLinks: {
-    title: string;
-    url: string;
-  }[];
-}
-
-export interface Notice {
-  message: string;
-}
-
-export interface Component {
-  id?: string;
-  title: string;
-  titleImageUrl: string;
-  summary: string;
-  description: string;
-}
-
 export type PrayerTime = {
   fajr: string;
   dhuhr: string;
